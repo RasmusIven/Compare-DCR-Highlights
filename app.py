a40159805a9faeb02253b89a606d083a59b4394b
Rasmus Iven Strømsted <rasmusiven@gmail.com>
2021-07-29 14:43:52 +0200

    code construction

--- app.py
+++ app.py
@@ -1,47 +1,20 @@
 import flask
 from flask import Flask, request, jsonify
-import xmltodict
+import xml.etree.ElementTree as ET
+import typing
+from typing import List, Set, Dict, Tuple, Optional
+import json
+
 
 #   COMPARE HIGHLIGHTS ALGORITHM
 #   Given two graphs, compare them by identifying annotation matches.
-#   Created 02.13.21, Rasmus Iven Strømsted, DCR Solutions
+#   Created 29.07.21, Rasmus Iven Strømsted, DCR Solutions
 
-#------------------------------------- Extract annotations from XML ---------------------------------------#
-def get_annos_from_xml(xml, annotation_type):
-    """
-    Given a Graph XML, extracts information about a specific annotation type, and return as dict.
-    """
-    highlights = []
-    lista = []
-    user_annos = {}
-    num_anno = 0
-    for highlight in xml["dcrgraph"]["specification"]["resources"]["custom"]["highlighterMarkup"]["highlights"]["highlight"]:
-        if highlight["@type"] == annotation_type:
-            anno = {}
-            for key, value in highlight["layers"]["layer"]["ranges"]["range"].items():
-                if key == "@start":
-                    anno['start'] = value
-                elif key == "@end":
-                    anno['end'] = value
-                elif key == "#text":
-                    anno['text'] = value
-            for key, value in highlight["items"]["item"].items():
-                if key == "@id":
-                    anno['id'] = value
-            user_annos[num_anno] = anno
-            num_anno = num_anno + 1
-    return user_annos
+#------------------------------------- Classes ---------------------------------------#
 
+def percentage(part, whole):
+    return 100 * float(part)/float(whole)
 
-def get_graphname_from_xml(xml):
-    """
-    Given a Graph XML, returns the title.
-    """
-    return xml["dcrgraph"]["@title"]
-
-
-
-#------------------------------------- Compare annotations Functions ---------------------------------------#
 def overlap(start1, end1, start2, end2):
     """
     Check if the ranges overlap.
@@ -49,77 +22,159 @@ def overlap(start1, end1, start2, end2):
     return (end1 >= start2) and (end2 >= start1)
 
 
-def compare_anno(anno_a, anno_b):
-    """
-    Given two annotations, checks if they are identical, or otherwise a overlap.
-    Returns a score of 0, 0.5 or 1
-    """
-    score = 0
-    if (anno_a['start'] == anno_b['start']) and (anno_a['end'] == anno_b['end']):
-        score = 1
-    elif overlap(anno_a['start'], anno_a['end'], anno_b['start'], anno_b['end']):
-        score = 0.5
+
+class Elem:
+    def __init__(self, type, ID):
+        self.type: str  = type
+        self.id: str = ID
+        self.highlights: list = []
+
+    def set_H(self, H):
+        self.highlights.append(H)
         
-    return score
+    def get_H(self) -> list:
+        return self.highlights
+    
+    def get_spans(self) -> list:
+        ranges = []
+        for H in self.highlights:
+            ranges.append(H.span())
+        return ranges
+
+    
+class H:
+    def __init__(self, start, end):
+        self.start: int = start
+        self.end: int = end
+    
+    def span(self) -> list:
+        return [self.start, self.end]
+
+    
+class Compare:
+    def __init__(self, source: list, target: list, types: list):
+        self.source = source
+        self.target = target
+        self.types = types
+        self.results = dict()
+    
+    def set_result(self, result:list, typ:str):
+        print(results)
+        print(typ)
+        self.results[typ] = result
+    
+    def get_score(self, typ) -> int:
+        source_count = len(self.source[f'{typ}'])
+        print(source_count)
+        #old_match_count = len(self.results[f'{typ}'])
+        score = 0
+        for match in self.results[f'{typ}']:
+            score = score + match['score']
+        print(score, source_count)
+        return percentage(score, source_count)
+
+    
+    
+class CompareSpans(Compare):
+    def give_score(self, aspan, bspan) -> int:
+        """
+        Given two annotations, checks if they are identical, or otherwise a overlap.
+        Returns a score of 0, 0.5 or 1
+        """
+        score = 0
+        if aspan == bspan:
+            score = 1
+        elif overlap(aspan[0], aspan[1], bspan[1], bspan[1]):
+            score = 0.75
+
+        return score
+
+    # given two lists of elements with highlights, find overlaps in highlights.
+    def overlaps(self, typ) -> list:
+        matches = []
+        for a in self.source[f'{typ}']:
+            match = False
+            for ah in a.highlights:
+                for b in self.target[f'{typ}']:
+                    for bh in b.highlights:
+                        if match == False and self.give_score(ah.span(), bh.span() ) > 0:
+                            match = True
+                            score = self.give_score(ah.span(), bh.span())
+                            matches.append({'score': score, 'matches': [a, b]})
+                            break;
+        return matches
 
 
-def compare_list_annos(dict_a, dict_b):
-    """
-    Takes two dict and compares each element in the list. If there is a match, the score is updated.
-    Returns score and list of annotation matches.
-    """
-    anno_matches = []
-    matches = []
-    t_score = (0, 0) # rating, count
+#------------------------------------- Import annotations from XML ---------------------------------------#
+
+def xml_extraction(xml, types) -> dict:
+    dict = {}
+    highlights = []
+    elements = []
+    root = xml
+    #root = tree.getroot()
     
-    for anno_a in dict_a.items():
-        matches_for_anno = {'anno': anno_a[1], 'matches': []}
-        for anno_b in dict_b.items():
-            score = compare_anno(anno_a[1], anno_b[1])
-            if score > 0:
-                
-                if anno_b in matches: # check if annotation already made:
-                    t_score = (t_score[0], t_score[1] + 1)
-        
-                else: # Add match
-                    matches.append(anno_b)
-                    t_score = (t_score[0] + score, t_score[1] + 1)
-                matches_for_anno['matches'].append(anno_b[1])
-                
-        anno_matches.append(matches_for_anno)
-    if t_score[0] == 0 or t_score[1] == 0:
-        r_score = 0
-    else:
-        r_score = (t_score[0]/t_score[1])*100
-    
-    return r_score
+    for type in types:
+        elements = []
+        for new_elem in root.findall(f".//highlights/highlight[@type='{type}']"):
+
+            # Find ID in XML
+            for child in new_elem.findall(".//items/item"):
+                Id = child.attrib['id']
+
+            # find existing or create element ID to add highlights to
+            match = False
+            for elem in elements:
+                if Id == elem.id:
+                    match = True
+                    addto = elem
+                    break;
+            # Create element:
+            if match == False:
+                addto = Elem(f'{type}', Id)
+                elements.append(addto)
+
+            # Add ranges as highlights to element
+            for range in new_elem.findall(".//layers/layer/ranges/"):
+                h = H(range.attrib['start'], range.attrib['end'])
+                if str(range.text) != "":
+                    h.text = range.text
+                highlights.append(h)
+                addto.set_H(h)
+        dict[f'{type}'] = elements
+    return dict
+
+
+#------------------------------------- Compare annotations Functions ---------------------------------------#
 
 
 #------------------------------------- REST Handler ---------------------------------------#
 app = flask.Flask(__name__)
+app.config["DEBUG"] = True
 
 @app.route('/', methods=['POST'])
 def home():
-    content_dict = xmltodict.parse(request.data)
-    compare_type = content_dict['content']['compare_types']
-    a_xml, b_xml = content_dict['content']['source_graph'], content_dict['content']['target_graph']
+
+    root = ET.fromstring(request.data)
+    types = []
+
+    for compare_type in root.findall(".//compare_types/type"):
+        types.append(compare_type.text)
+    print(types)
+    for graph in root.findall('.//source_graph/*'):
+        print(graph)
+        source = xml_extraction(graph, types)
+
+    for graph in root.findall('.//target_graph/*'):
+        target = xml_extraction(graph, types)
     
-    results = {}
-    results['final_score'] = 0
-    counter = 0
-    for typ in compare_type['type']:
-        extract_1 = get_annos_from_xml(a_xml, typ)
-        extract_2 = get_annos_from_xml(b_xml, typ)
-        
-        results[typ] = {'source': extract_1, 'target': extract_2}
-        results[typ]['score'] = compare_list_annos(extract_2, extract_1)
-        
-        results['final_score'] = results['final_score'] + results[typ]['score']
-        if results[typ]['score'] != 0:
-            counter = counter + 1
+    matcher = CompareSpans(source, target, types)
+    matcher.results['role'] = matcher.overlaps('role')
+    print('score: ', matcher.get_score('role'))
+
+    score = matcher.get_score('role')
+    return json.dumps(score)
     
-    if results['final_score'] != 0:
-        results['final_score'] = results['final_score'] / counter
-    
-    return results  
         
+
+app.run()
